f63b16d7ea2615ace6005e4dc5833e957055b120
kjayru <wiltinoco@gmail.com>
2019-02-05 09:19:12 -0500

    vistas

--- resources/views/layouts/front/app.blade.php
+++ resources/views/layouts/front/app.blade.php
@@ -4,28 +4,18 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>{{ env('APP_NAME')}}</title>
-
 	<link rel="stylesheet" type="text/css" href="/css/main.css?v={{ uniqid() }}">
 	<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
-
+    <link rel="shortcut icon" href="/favicon.ico" />
 </head>
 <body>
-
 <!--inc header-->
 @include('layouts.front.partial.header')
 
-
-
 @yield('content')
-
-
-
   <!--inc footer-->
   @include('layouts.front.partial.footer')
-
-
   <!-- inc scripts-->
-
   @include('layouts.front.partial.scripts')
 </body>
 </html>
