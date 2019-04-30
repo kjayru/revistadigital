739761b86343d0e31c338e2f7298647989f74c58
kjayru <wiltinoco@gmail.com>
2019-04-29 20:44:20 -0500

    fix

---
+++ app/Listeners/SuccessfulLogin.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Session;
+class SuccessfulLogin
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Login  $event
+     * @return void
+     */
+    public function handle(Login $event)
+    {
+        $ipAddress = '';
+
+        
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && ('' !== trim($_SERVER['HTTP_X_FORWARDED_FOR']))) {
+            $ipAddress = trim($_SERVER['HTTP_X_FORWARDED_FOR']);
+        } else {
+            if (isset($_SERVER['REMOTE_ADDR']) && ('' !== trim($_SERVER['REMOTE_ADDR']))) {
+                $ipAddress = trim($_SERVER['REMOTE_ADDR']);
+            }
+        }
+        $session = new Session();
+        $session->user_id = $event->user->id;
+        $session->ip_address = $ipAddress;
+        $session->user_agent = $_SERVER['HTTP_USER_AGENT'];
+        $session->last_activity = new \DateTime;
+        $session->save();
+    }
+}
--- app/User.php
+++ app/User.php
@@ -33,7 +33,7 @@ class User extends Authenticatable
     public static function existuser($email){
         $user = \App\User::where('email',$email)->count();
         $usuarios = \App\User::all();
-
-
     }
+
+   
 }
